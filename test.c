5d0a46203643e56665b3a5a9380f35c94b8bdb9b
Amine Tahiri Alaoui <atahiri-@e2r3p15.1337.ma>
2025-10-24 20:45:48 +0100

    Fixed: the double printing in the test

--- test.c
+++ test.c
@@ -11,12 +11,16 @@ void test_one_char_arg(void)
 	char args[] = { 'A',        'A',        'A',			  'A'};
 	char out[1024], expected[1024];
 	int fd1, fd2;
+	int ret1, ret2;
 
 	for (int i = 0; i < sizeof(fmts) / sizeof(*fmts); i++)
 	{
+		TEST_INFO(fmts[i]);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
 		fd2 = open("/tmp/test_ft_printf2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		ASSERT_EQ(ft_dprintf(fd1, fmts[i], args[i]), dprintf(fd2, fmts[i], args[i]));
+		ret1 = ft_dprintf(fd1, fmts[i], args[i]);
+		ret2 = dprintf(fd2, fmts[i], args[i]);
+		ASSERT_EQ(ret1, ret2);
 		close(fd1), close(fd2);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_RDONLY);
 		fd2 = open("/tmp/test_ft_printf2.txt", O_RDONLY);
