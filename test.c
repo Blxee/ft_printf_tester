f9e11676b187ffd2689654315ac52d0fa1f08676
Blxee <aminedev57@gmail.com>
2025-11-07 10:36:56 +0100

    Added: more thorough tests

--- test.c
+++ test.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atahiri- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/07 08:54:23 by atahiri-          #+#    #+#             */
+/*   Updated: 2025/11/07 10:36:20 by atahiri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "castrum/castrum.h"
 #include "ft_printf/ft_printf.h"
 #include <stdio.h>
@@ -5,10 +17,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void test_normal_str(void)
+void test_str(void)
 {
-	char *fmts[] = {"hello %s", "%s hello", "hello %s world", "%   ",    "hello %s"};
-	char *args[] = {"world",    "hey",      "there",		  "hello", NULL};
+	char *fmts[] = { "%s",    "hello %s", "%s hello", "hello %s world", "%s  ",  "  %s",     "hello %s", "%s hello", "%s", "%s%s",  "hello %s %s", "%s %s hello", "hello %s %s world", "%s%s  ",  "  %s%s",     "hello %s %s", "%s %s hello", "%s%s", "%s%s%s", "hello %s from %s far %s!"};
+	char *args1[] = {"world", "",         "world",    "world",          "world", "world",    NULL,       NULL,       NULL, "world", "world",       "world",       "world",             "world",   "world",      NULL,          "hey",         NULL,   "hello",  "world"};
+	char *args2[] = {NULL,    NULL,       NULL,       NULL,             NULL,    NULL,       NULL,       NULL,       NULL, "hey",   "",            "hey",         "hey",               "hey",     "hey",        "hey",         NULL,          NULL,   "world",  "somewhere"};
+	char *args3[] = {NULL,    NULL,       NULL,       NULL,             NULL,    NULL,       NULL,       NULL,       NULL, NULL,    NULL,          NULL,          NULL,                NULL,      NULL,         NULL,          NULL,          NULL,   "hey",    "away"};
 	char out[1024], expected[1024];
 	int fd1, fd2;
 	int ret1, ret2;
@@ -18,8 +32,8 @@ void test_normal_str(void)
 		// TEST_INFO(fmts[i]);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
 		fd2 = open("/tmp/test_ft_printf2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		ret1 = ft_dprintf(fd1, fmts[i], args[i]);
-		ret2 = dprintf(fd2, fmts[i], args[i]);
+		ret1 = ft_dprintf(fd1, fmts[i], args1[i], args2[i], args3[i]);
+		ret2 = dprintf(fd2, fmts[i], args1[i], args2[i], args3[i]);
 		ASSERT_EQ(ret1, ret2);
 		close(fd1), close(fd2);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_RDONLY);
@@ -31,10 +45,12 @@ void test_normal_str(void)
 	}
 }
 
-void test_one_char_arg(void)
+void test_char(void)
 {
-	char *fmts[] = {"hello %c", "%c hello", "hello %c world", "%c"};
-	char args[] = { 'A',        'A',        'A',			  'A'};
+	char *fmts[] = {"hello %c", "%c hello", "hello %c world", "%c", "hello %c%c", "%c%c hello", "%c hello %c", "  %c  %c  ", "%c%c%%c", "hello %c from %c far %c away"};
+	char args1[] = {'A',        'A',        'A',			  'A',  'A',          'A',          'A',		   'A',          'A',       'A',};
+	char args2[] = {0,          0,          0,			      0,    'B',          'B',          'B',	       'B',          'B',       'B',};
+	char args3[] = {0,          0,          0,			      0,    0,            0,            0,			   0,            'C',       'C',};
 	char out[1024], expected[1024];
 	int fd1, fd2;
 	int ret1, ret2;
@@ -44,8 +60,8 @@ void test_one_char_arg(void)
 		// TEST_INFO(fmts[i]);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
 		fd2 = open("/tmp/test_ft_printf2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		ret1 = ft_dprintf(fd1, fmts[i], args[i]);
-		ret2 = dprintf(fd2, fmts[i], args[i]);
+		ret1 = ft_dprintf(fd1, fmts[i], args1[i], args2[i], args3[i]);
+		ret2 = dprintf(fd2, fmts[i], args1[i], args2[i], args3[i]);
 		ASSERT_EQ(ret1, ret2);
 		close(fd1), close(fd2);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_RDONLY);
@@ -57,10 +73,12 @@ void test_one_char_arg(void)
 	}
 }
 
-void test_one_pointer_arg(void)
+void test_pointer(void)
 {
-	char *fmts[] = {"hello %p", "hello %p"};
-	void *args[] = {0xffffffff, NULL};
+	char *fmts[] = { "%p",         "hello %p",   "%p hello",   "hello %p world", "  %p  ",     "%p", "hello %p", "%p hello", "hello %p world", "  %p  ", "%p%p",       "hello %p %p", "%p %p hello", "%p hello %p", "%p%p%p",     "hello %p from %p far %p away", "hello %p%p%p", "%p%p%p hello", " %p_%p %p_",  "%p%p%p", " %p hi %phi%phi"};
+	void *args1[] = {&(char){'A'}, &(char){'A'}, &(char){'A'}, &(char){'A'},     &(char){'A'}, NULL, NULL,       NULL,       NULL,             NULL,     &(char){'A'}, &(char){'A'},  &(char){'A'},  &(char){'A'},  &(char){'A'}, &(char){'A'},                   &(char){'A'},   &(char){'A'},   &(char){'A'},  NULL,     NULL};
+	void *args2[] = {NULL,         NULL,         NULL,         NULL,             NULL,         NULL, NULL,       NULL,       NULL,             NULL,     &(char){'A'}, &(char){'A'},  &(char){'A'},  &(char){'A'},  &(char){'A'}, &(char){'A'},                   &(char){'A'},   &(char){'A'},   &(char){'A'},  NULL,     NULL};
+	void *args3[] = {NULL,         NULL,         NULL,         NULL,             NULL,         NULL, NULL,       NULL,       NULL,             NULL,     NULL,         NULL,          NULL,          NULL,          &(char){'A'}, &(char){'A'},                   &(char){'A'},   &(char){'A'},   &(char){'A'},  NULL,     NULL};
 	char out[1024], expected[1024];
 	int fd1, fd2;
 	int ret1, ret2;
@@ -70,8 +88,8 @@ void test_one_pointer_arg(void)
 		// TEST_INFO(fmts[i]);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
 		fd2 = open("/tmp/test_ft_printf2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		ret1 = ft_dprintf(fd1, fmts[i], args[i]);
-		ret2 = dprintf(fd2, fmts[i], args[i]);
+		ret1 = ft_dprintf(fd1, fmts[i], args1[i], args2[i], args3[i]);
+		ret2 = dprintf(fd2, fmts[i], args1[i], args2[i], args3[i]);
 		ASSERT_EQ(ret1, ret2);
 		close(fd1), close(fd2);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_RDONLY);
@@ -83,13 +101,12 @@ void test_one_pointer_arg(void)
 	}
 }
 
-void test_one_int_arg(void)
+void test_int(void)
 {
-	// char *fmts[] = { "hello %d", "hello %+d", "hello % d", "hello %0d", "hello %-d", "hello %.d", "hello %.0d", "hello %5d", "hello %-5d", "hello %05d", "hello %+5d", "hello % 5d", "hello %-+5d", "hello %- 5d", "hello %.3d", "hello %.5d", "hello %.10d", "hello %8.3d", "hello %8.5d", "hello %-8.5d", "hello %+8.5d", "hello % 8.5d", "hello %8.0d", "hello %.0d", "hello %08.0d", "value=%+10.5d end", "x=%- 10.3d y", "%+010.3d middle", "test: [% .5d]", "hello %u", "hello %05u", "hello %-5u", "hello %.5u", "hello %8.5u", "hello %08.5u", };
-	// int args[] = {   0,          123,         -123,        42,          -42,         99999,       -99999,       1,           -1,           1000,         -1000,        12,           123,           4567,          -4567,        0,            987654321,     -987654321,    10,            0,              123,            -123,           255,           -255,         40000,          0,                   123,            123,               123,             123,        123,          123,          123,           -123,         2002342, };
-
-	char *fmts[] = {"hello %d", "hello %u", "hello %d", "hello %-12d", "hello %010d", "hello %-010d", "hello %+d", "%+d hello"};
-	int args[] = {  123,        123,        -123,       12,            123,           123,            123,         -123};
+	char *fmts[] = {"%d", "%d", "%d", "%d",        "%d",       "hello %d", "hello %d", "hello %d", "hello %d",  "hello %d", "%d hello", "%d hello", "%d hello", "%d hello",  "%d hello", "hello %d world", "hello %d world", "hello %d world", "hello %d world", "hello %d world", "%d%d",      "hello %d %d", "%d %d hello", "hello %d from %d far", " %d  %d ",  "%d%d%d",   "hello %d from %d far %d place", "he%dll%do%do"};
+	int args1[] = { 123,  -123, 0,    -2147483648, 2147483647, 123,        -123,       0,          -2147483648, 2147483647, 123,        -123,       0,          -2147483648, 2147483647, 123,              -123,             0,                -2147483648,      2147483647,       123,         -123,          0,             2147483647,             -2147483648, 123,        0,                               -123};
+	int args2[] = {0,     0,    0,    0,           0,          0,          0,          0,          0,           0,          0,          0,          0,          0,           0,          0,                0,                0,                0,                0,                -2147483648, 0,             2147483647,    123,                    -123,        -123,       -2147483648,                     0};
+	int args3[] = {0,     0,    0,    0,           0,          0,          0,          0,          0,           0,          0,          0,          0,          0,           0,          0,                0,                0,                0,                0,                0,           0,             0,             0,                      0,           2147483647, 123,                             2147483647};
 	char out[1024], expected[1024];
 	int fd1, fd2;
 	int ret1, ret2;
@@ -99,8 +116,8 @@ void test_one_int_arg(void)
 		// TEST_INFO(fmts[i]);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
 		fd2 = open("/tmp/test_ft_printf2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-		ret1 = ft_dprintf(fd1, fmts[i], args[i]);
-		ret2 = dprintf(fd2, fmts[i], args[i]);
+		ret1 = ft_dprintf(fd1, fmts[i], args1[i], args2[i], args3[i]);
+		ret2 = dprintf(fd2, fmts[i], args1[i], args2[i], args3[i]);
 		ASSERT_EQ(ret1, ret2);
 		close(fd1), close(fd2);
 		fd1 = open("/tmp/test_ft_printf1.txt", O_RDONLY);
@@ -112,7 +129,7 @@ void test_one_int_arg(void)
 	}
 }
 
-void test_one_hex_arg(void)
+void test_hex(void)
 {
 	char *fmts[] = {"hello %x", "hello %X"};
 	long args[] = { 0xffffffff, 0xffffffff};
@@ -138,7 +155,7 @@ void test_one_hex_arg(void)
 	}
 }
 
-void test_one_percent(void)
+void test_percent(void)
 {
 	char *fmts[] = {"hello %%", "hello %%"};
 	char out[1024], expected[1024];
@@ -165,10 +182,10 @@ void test_one_percent(void)
 
 int main(void)
 {
-	RUN_TEST(test_one_char_arg);
-	RUN_TEST(test_normal_str);
-	RUN_TEST(test_one_int_arg);
-	RUN_TEST(test_one_pointer_arg);
-	RUN_TEST(test_one_hex_arg);
-	RUN_TEST(test_one_percent);
+	RUN_TEST(test_char);
+	RUN_TEST(test_str);
+	RUN_TEST(test_int);
+	RUN_TEST(test_pointer);
+	RUN_TEST(test_hex);
+	RUN_TEST(test_percent);
 }
